446c4e14afb49cb367d79fe11fb26ead2d86ca77
Chenentano <39513916+Chenentano@users.noreply.github.com>
2021-07-26 12:34:07 +0200

    changes

---
+++ übung.js
@@ -0,0 +1,24 @@
+/**
+ * @Chenentano
+ */
+
+function Simon(){
+
+  let simon = {
+              Name:"Simon",
+              Skill:"Quasi nicht vorhanden",
+              Statur:"Lauch",
+              Rapskills:"WhackMC"
+            }
+
+  var meinungZuSimon;
+  meinungZuSimon = 
+              "Durch gründlicher Analyse der Daten\n\n"+
+              "Name: "  +simon.Name + "\n" +
+              "Skill: " +simon.Skill + "\n" +
+              "Statur: " +simon.Statur + "\n" +
+              "Rapskills: " +simon.Rapskills + "\n\n" +
+              "bin ich zu dem Entschluss gekommen, das er ein Kelb ist.\nLG";
+
+  alert (meinungZuSimon);
+}
